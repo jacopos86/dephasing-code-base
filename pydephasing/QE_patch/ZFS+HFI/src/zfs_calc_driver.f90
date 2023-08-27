37e4347708a33d6311052816fd6c59976bd1b457
jacopos86 <simonij@tcd.ie>
2023-08-27 12:16:13 -0400

    QE patch (2)

--- pydephasing/QE_patch/ZFS+HFI/src/zfs_calc_driver.f90
+++ pydephasing/QE_patch/ZFS+HFI/src/zfs_calc_driver.f90
@@ -9,8 +9,13 @@ PROGRAM zfs_calculation
   ! ----------------------------------------------------------------------------
   !
   
-  USE io_files,                  ONLY : prefix
+  USE io_files,                  ONLY : prefix, tmp_dir
   USE mp_global,                 ONLY : mp_startup
+  USE mp_images,                 ONLY : intra_image_comm
+  USE io_global,                 ONLY : ionode, ionode_id
+  USE mp_pools,                  ONLY : npool
+  USE mp,                        ONLY : mp_bcast
+  USE environment,               ONLY : environment_start, environment_end
   
   
   
@@ -21,7 +26,10 @@ PROGRAM zfs_calculation
   !
   
   CHARACTER (LEN=256)              :: outdir
-  
+  CHARACTER (LEN=8)                :: code = 'ZFS_CALC'
+  CHARACTER (LEN=256)              :: trimcheck
+  !
+  INTEGER                          :: ios
   
   
   
@@ -34,25 +42,55 @@ PROGRAM zfs_calculation
 #ifdef __MPI
   call mp_startup ()
 #endif
-  
-  
-  
-  
-  
-  
-  
 
-
-
-
-
-
-
-
-
-
-  
   !
+  call environment_start (code)
+
+  !
+  prefix = 'zfs'
+  call get_environment_variable ('ESPRESSO_TMPDIR', outdir)
+  IF ( TRIM (outdir) == ' ' ) outdir = './'
+
+  !
+  IF (npool > 1) call errore (code, 'pools not implemented', npool)
+  !
+  IF (ionode) THEN
+     !
+     call input_from_file ()
+     !
+     READ (5, inputzfs_hfi, err=200, iostat=ios)
+200  call errore (code, 'reading inputzfs_hfi namelist', abs (ios))
+
+     !
+     tmp_dir = trimcheck (outdir)
+     !
+  END IF
+
+  !
+  !  ... bcast
+  !
+
+  call mp_bcast (tmp_dir, ionode_id, intra_image_comm)
+  call mp_bcast (prefix, ionode_id, intra_image_comm)
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  call environment_end (code)
+  !
+  
+  call stop_pp
   STOP
   
   !
