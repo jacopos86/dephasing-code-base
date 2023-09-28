48a18d09a37a764cd346230380e99f7a0dca3431
jacopos86 <simonij@tcd.ie>
2023-09-27 23:03:00 -0400

    acf 2 update(4)

--- pydephasing/gpu_source/compute_acf_V2.cu
+++ pydephasing/gpu_source/compute_acf_V2.cu
@@ -2,7 +2,7 @@
 #define PI 3.141592653589793
 typedef pycuda::complex<double> cmplx;
 
-__global__ void compute_acf_V2_oft(int *qlp_init, int *lgth, int *qlp_lst, const int t_size,
+__global__ void compute_acf_V2_oft(int *qlp_init, int *lgth, int *qlp_lst, const int SIZE,
 double *time, double wq, double *wqp, double wuq, double *wuqp, double Alq, double *Alqp, 
 cmplx *Flqlqp, double T, double DE, double NU, double MINFREQ, double THZTOEV, double KB, 
 const double TOLER, cmplx *acf, cmplx *acf_int) {
@@ -12,40 +12,36 @@ const double TOLER, cmplx *acf, cmplx *acf_int) {
     //printf("%d ", blockIdx.z);
     int idx = i + j * blockDim.x * gridDim.x + k * blockDim.x * gridDim.x * blockDim.y * gridDim.y;
     int tx  = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;
-    int ipx = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
-    int imp, ii0, ii, nmp;
-    double x, nlqp, Eqlp;
-    double re, im;
-    double A1, A2, A3;
-    cmplx ft;
-    if (tx < t_size) {
-        ii0 = qlp0_lst[ipx];
-        nmp = lgth_lst[ipx];
-        for (ii=ii0; ii<ii0+nmp; ii++) {
-            imp = qlp_lst[ii]; 
-            if (wuq[imp] > min_freq) {
-                Eqlp = wuq[imp] * THz_to_ev;
-                re = cos(2.*PI*wuq[imp]*time[tx]);
-                im = sin(2.*PI*wuq[imp]*time[tx]);
-                cmplx eiwpt(re, -im);
-                cmplx cc_eiwpt = conj(eiwpt);
-                if (T < toler) {
-                    nlqp = 0.;
-                }
-                else {
-                    x = Eqlp / (kb * T);
-                    if (x > 100.) {
-                        nlqp = 0.;
-                    }
-                    else {
-                        nlqp = 1./(exp(x) - 1.);
-                    }
-                }
-                A1 = 1. + nlq + nlqp + nlq * nlqp;
-                A2 = nlq * nlqp;
-                A3 = 2.*(1. + nlq) * nlqp;
-                ft = A1 * eiwt[tx] * eiwpt + A2 * cc_eiwpt * conj(eiwt[tx]) + A3 * eiwt[tx] * cc_eiwpt * eint[tx];
-                acf[idx] += wq * wqp[imp] * Alq * Alq * Alqp[imp] * Alqp[imp] * ft * Flqp[imp] * conj(Flqp[imp]);
+    int iqlx= blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
+    /* internal variables */
+
+    if (tx < SIZE) {
+        iqlp0 = qlp_init[iqlx];
+        n = lgth[iqlx];
+        wql = 2.*PI*wuq;
+        /* ph. occup. */
+        Eql = wuq * THZTOEV;
+        x = Eql / (KB * T);
+        nql = bose_occup(x, T, TOLER);
+        /* run over (q',l') */
+        for (ii=iqlp0; ii<iqlp0+n; ii++) {
+            iqlp = qlp_lst[ii]; 
+            if (wuqp[iqlp] > MINFREQ) {
+                wqlp = 2.*PI*wuqp[iqlp];
+                Eqlp = wuqp[iqlp] * THZTOEV;
+                /* set e^iwt */
+                re = cos((DE + wqlp - wql)*time[tx]);
+                im = sin((DE + wqlp - wql)*time[tx]);
+                cmplx eiwt(re, -im);
+                /* ph. occup.*/
+                x = Eqlp / (KB * T);
+                nqlp = bose_occup(x, T, TOLER);
+                /* acf^2(t) */
+                ft = nql * (1.+nqlp) * eiwt * EXP(-NU*time[tx]);
+                acf[idx] += wq * wqp[iqlp] * Alq * Alq * Alqp[iqlp] * Alqp[iqlp] * ft * Flqlqp[iqlp] * conj(Flqlqp[iqlp]);
+                /* \int acf^2(t) */
+                ft(0.,0.);
+                ft = nql * (1.+nqlp) * eiwt * EXP(-NU*time[tx]);
             }
         }
     }
