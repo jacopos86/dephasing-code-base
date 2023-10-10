2d7963a83cc073884091c58e4e582253e389bba5
jacopos86 <simonij@tcd.ie>
2023-10-09 23:56:03 -0400

    acf 2 update(8)

--- pydephasing/gpu_source/compute_acf_V2.cu
+++ pydephasing/gpu_source/compute_acf_V2.cu
@@ -122,9 +122,65 @@ cmplx *acf, cmplx *acf_int) {
                     wqlp = 2.*PI*wuqp[iqlp];
                     Eqlp = wuqp[iqlp] * THZTOEV;
                     /* EXP ^ iwt */
-                    
+                    re = cos((DE + wqlp - wql)*time[tx]);
+                    im = sin((DE + wqlp - wql)*time[tx]);
+                    cmplx eiwt(re, -im);
+                    /* ph. occup. */
+                    x = Eqlp / (KB * T);
+                    nqlp = bose_occup(x, T, TOLER);
+                    /* acf^(2)(t) */
+                    ft = nql * (1.+nqlp) * eiwt * EXP(-NU*time[tx]);
+                    for (dx=0; dx<3; dx++) {
+                        iFx = 3*NAT*iqlp+3*ia+dx;
+                        acf[idx] += wq * wqp[iqlp] * Alq * Alq * Alqp[iqlp] * Alqp[iqlp] * ft * Fjax_lqlqp[iFx] * conj(Fjax_lqlqp[iFx]);
+                    }
+                    /* compute cumulative sum auto correl. function (eV^2 ps) units*/
+                    cmplx DN(DE+wqlp-wql,-NU);
+                    ft = IU * nql * (1.+nqlp) * (eiwt * EXP(-NU*time[tx]) - 1.) / DN
+                    for (dx=0; dx<3; dx++) {
+                        iFx = 3*NAT*iqlp+3*ia+dx;
+                        acf_int[idx] += wq * wqp[iqlp] * Alq * Alq * Alqp[iqlp] * Alqp[iqlp] * ft * Fjax_lqlqp[iFx] * conj(Fjax_lqlqp[iFx]);
+                    }
                 }
             }
         }
     }
+}
+
+__global__ void compute_acf_V2_atr_ofw(int *at_lst, int NA_SIZE, double *wg, const int SIZE, int NMODES,
+int NAT, double DE, double ETA, double wq, double wuq, double Alq, double *wqp, double *wuqp,
+double *Alqp, cmplx *Fjax_lqlqp, double T, double MINFREQ, double THZTOEV, double KB, double TOLER, cmplx *acfw) {
+    const int i = threadIdx.x + blockDim.x * blockIdx.x;
+    const int j = threadIdx.y + blockDim.y * blockIdx.y;
+    const int k = threadIdx.z + blockDim.z * blockIdx.z;
+    int idx = i + j * blockDim.x * gridDim.x + k * blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+    int iwx = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;
+    int ax = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
+    /* internal vars. */
+
+
+
+    /* tx < SIZE : PROCEED */
+    if (tx < SIZE && ax < NA_SIZE) {
+        int ia = at_lst[ax];
+        if (wuq > MINFREQ) {
+            wql = 2.*PI*wuq;
+            /* ph. occup. */
+            Eql = wuq * THZTOEV;
+            x = Eql / (KB * T);
+            nql = bose_occup(x, T, TOLER);
+            /* iterate over (q',l')*/
+            for (iqlp=0; iqlp<NMODES; iqlp++) {
+                if (wuqp[iqlp] > MINFREQ) {
+                    wqlp = 2.*PI*wuqp[iqlp];
+                    Eqlp = wuqp[iqlp] * THZTOEV;
+                    /* ph. occup. number */
+                    x = Eqlp / (KB * T);
+                    nqlp = bose_occup(x, T, TOLER);
+                }
+            }
+        }
+    }
+
+
 }
