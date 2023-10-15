60ba44d9413f3570ba4205f2cfcb264c9fd8f596
jacopos86 <simonij@tcd.ie>
2023-10-15 11:02:55 -0400

    update QE(4)

--- QE_to_pydeph.sh
+++ QE_to_pydeph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/*.f90 ./pydephasing/QE_patch/ZFS+HFI/src/
-cp ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/Makefile ./pydephasing/QE_patch/ZFS+HFI/src/
-cp ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/make.depend ./pydephasing/QE_patch/ZFS+HFI/src/ 
+cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/*.f90 ./pydephasing/QE_patch/QE2pydeph/src/
+cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/Makefile ./pydephasing/QE_patch/QE2pydeph/src/
+cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/make.depend ./pydephasing/QE_patch/QE2pydeph/src/ 
--- pydeph_to_QE.sh
+++ pydeph_to_QE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ./pydephasing/QE_patch/ZFS+HFI/src/*.f90 ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/
-cp ./pydephasing/QE_patch/ZFS+HFI/src/Makefile ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/
-cp ./pydephasing/QE_patch/ZFS+HFI/src/make.depend ~/PROJECTS/q-e-qe-6.5/ZFS+HFI/src/
+cp ./pydephasing/QE_patch/QE2pydeph/src/*.f90 ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/
+cp ./pydephasing/QE_patch/QE2pydeph/src/Makefile ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/
+cp ./pydephasing/QE_patch/QE2pydeph/src/make.depend ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/
