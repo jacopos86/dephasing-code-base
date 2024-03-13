fb01191b7465053b8b59b55879f871e15c93edd4
jacopos86 <simonij@tcd.ie>
2024-03-13 02:40:01 -0400

    update SOCNC

--- clean_vers_files/QE_patch/QE2pydeph/src/spin_orbit_operator_ncpp.f90
+++ clean_vers_files/QE_patch/QE2pydeph/src/spin_orbit_operator_ncpp.f90
@@ -18,13 +18,22 @@ MODULE spin_orbit_operator_ncpp
     !
     CONTAINS
     !
-    ! ==========================================================
-    SUBROUTINE compute_Vso_GGp ( )
-        ! ------------------------------------------------------
+    ! =========================================================================
+    SUBROUTINE compute_Vso_nnp ( )
+        ! ---------------------------------------------------------------------
+        !
+        !  fSO(G,G') = \sum_pm (pm ihbar) \sum_l 2/(2l+1) |beta_pm^l(G)>
+        !         (delta E_pm^l)^-1 <beta_pm^l(G')| Pl'(G,G') GXG'/|G|/|G'|
+        !
+        !  VSO(n,n') = \sum_G,G' <n,G|fSO(G,G')|n',G'>
 
-
+        IMPLICIT NONE
+        
         !
         !   internal variables
+
+
+
 
 
 
@@ -45,10 +54,30 @@ MODULE spin_orbit_operator_ncpp
         !
         RETURN
         !
-    END SUBROUTINE compute_Vso_GGp
+    END SUBROUTINE compute_Vso_nnp
+
+    !
+    ! ==============================================
+    SUBROUTINE compute_Pl_derivative ()
+        ! ------------------------------------------
+
+
+        !
+        IMPLICIT NONE
+
+        !
+        !   internal variables
 
 
 
 
+
+        
+
+        
+        !
+        RETURN
+        !
+    END SUBROUTINE compute_Pl_derivative
     !
 END MODULE spin_orbit_operator_ncpp
