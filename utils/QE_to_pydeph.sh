bfb9e8de013bf3dbe18c74040b317988a0d10588
Jacopo Simoni <simonij@tcd.ie>
2025-03-02 00:00:13 -0600

    removed these utilities files not needed

--- utils/QE_to_pydeph.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/*.f90 ./pydephasing/QE_patch/QE2pydeph/src/
-cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/Makefile ./pydephasing/QE_patch/QE2pydeph/src/
-cp ~/PROJECTS/q-e-qe-6.5/QE2pydeph/src/make.depend ./pydephasing/QE_patch/QE2pydeph/src/ 
