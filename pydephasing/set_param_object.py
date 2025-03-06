6a3e4269a03d3d2fd3e8d6e5247eaed4863e64a7
Jacopo Simoni <simonij@tcd.ie>
2025-03-06 07:50:03 -0600

    update parameter definition

--- pydephasing/set_param_object.py
+++ pydephasing/set_param_object.py
@@ -6,9 +6,9 @@ calc_type1 = parser.parse_args().ct1[0]
 if calc_type1 == "init":
     p = preproc_data_input()
 else:
-    deph_type = parser.parse_args().typ
-    if deph_type == "stat" or deph_type == "statdd":
-        p = static_data_input()
-    elif deph_type == "relax" or deph_type == "deph":
+    ct2 = parser.parse_args().ct2
+    if ct2 == "inhomo" or ct2 == "homo" or ct2 == "full":
         p = dynamical_data_input()
+    elif ct2 == "stat" or ct2 == "statdd":
+        p = static_data_input()
 p.sep = "*"*94
