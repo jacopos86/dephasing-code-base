74e017d0b0423a1cdf6af60447a8acd8edbab967
jacopos86 <simonij@tcd.ie>
2025-03-30 08:42:52 -0500

    set_cross_prod_matrix + norm_realv

--- pydephasing/common/matrix_operations.py
+++ pydephasing/common/matrix_operations.py
@@ -1,8 +1,28 @@
 import numpy as np
 
+#
+#  vector norm
+#
+def norm_realv(v):
+    nrm = np.sqrt(sum(v[:]*v[:]))
+    return nrm
+
 # compute matrix elements
 # <qs1|A|qs2>
 def compute_matr_elements(A, s1, s2):
     r = np.einsum("ij,j->i", A, s2)
     expv = np.einsum("i,i", s1.conjugate(), r)
     return expv
+
+#
+#   function: set cross product matrix
+#
+def set_cross_prod_matrix(a):
+    A = np.zeros((3,3))
+    A[0,1] = -a[2]
+    A[0,2] =  a[1]
+    A[1,0] =  a[2]
+    A[1,2] = -a[0]
+    A[2,0] = -a[1]
+    A[2,1] =  a[0]
+    return A
