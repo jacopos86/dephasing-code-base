7eee086a8fdd54589cd9dff5b49a6ac2d21e5f62
jacopos86 <simonij@tcd.ie>
2025-03-30 09:13:07 -0500

    replace expv->mel

--- pydephasing/common/matrix_operations.py
+++ pydephasing/common/matrix_operations.py
@@ -11,8 +11,8 @@ def norm_realv(v):
 # <qs1|A|qs2>
 def compute_matr_elements(A, s1, s2):
     r = np.einsum("ij,j->i", A, s2)
-    expv = np.einsum("i,i", s1.conjugate(), r)
-    return expv
+    mel = np.einsum("i,i", s1.conjugate(), r)
+    return mel
 
 #
 #   function: set cross product matrix
