f46c0df42156b161706b75d8d15fc1f077d136f8
jacopos86 <simonij@tcd.ie>
2025-04-13 12:37:55 -0500

    new file

---
+++ pydephasing/compute_LR_spin_decoher.py
@@ -0,0 +1,172 @@
+# This is the main subroutine for
+# the calculation of the ZFS dephasing
+# it computes the energy autocorrelation function
+# and return it for further processing
+import logging
+import numpy as np
+import os
+from pydephasing.mpi import mpi
+from pydephasing.log import log
+from pydephasing.set_param_object import p
+from pydephasing.atomic_list_struct import atoms
+from pydephasing.spin_hamiltonian import spin_triplet_hamiltonian
+from pydephasing.spin_ph_inter import SpinPhononClass
+from pydephasing.ph_amplitude_module import PhononAmplitude
+from pydephasing.auto_correlation_spph_mod import acf_sp_ph
+from pydephasing.T2_calc_handler import set_T2_calc_handler
+from pydephasing.energy_fluct_mod import ZFS_ph_fluctuations
+from pydephasing.phonons_module import PhononsClass
+from pydephasing.q_grid import qgridClass
+from pydephasing.build_interact_grad import calc_interaction_grad
+#
+def compute_spin_dephas(ZFS_CALC, HFI_CALC):
+    # main driver code for the calculation of dephasing time
+    # in homogeneous spin systems
+    #
+    # first set up atoms
+    # compute index maps
+    atoms.set_atoms_data()
+    # check restart exist otherwise create
+    if not os.path.isdir(p.work_dir+'/restart'):
+        if mpi.rank == mpi.root:
+            os.mkdir(p.work_dir+'/restart')
+    mpi.comm.Barrier()
+    # extract interaction gradients
+    interact_dict = calc_interaction_grad(ZFS_CALC, HFI_CALC)
+    mpi.comm.Barrier()
+    # n. atoms
+    nat = atoms.nat
+    # extract unperturbed struct.
+    if interact_dict['gradZFS'] is not None:
+        struct_0 = interact_dict['gradZFS'].struct_0
+    # set up the spin Hamiltonian
+    Hsp = spin_triplet_hamiltonian()
+    Hsp.set_zfs_levels(struct_0, p.B0)
+    # set up spin phonon interaction class
+    sp_ph_inter = SpinPhononClass().generate_instance(p.order_2_correct, ZFS_CALC, HFI_CALC)
+    if mpi.rank == mpi.root:
+        log.debug("\t ZFS_CALC: " + str(sp_ph_inter.ZFS_CALC))
+        log.debug("\t HFI_CALC: " + str(sp_ph_inter.HFI_CALC))
+    # set q grid
+    qgr = qgridClass()
+    qgr.set_qgrid()
+    if mpi.rank == 0:
+        log.info("\n")
+        log.info("\t " + p.sep)
+        log.info("\t Q MESH INFORMATION")
+        log.info("\n")
+        log.info("\n")
+        log.info("\t nq: " + str(qgr.nq))
+        log.info("\t grid size: " + str(qgr.grid_size))
+        if qgr.nq > 10:
+            for iq in range(10):
+                log.info("\t wq[" + str(iq+1) + "]: " + str(qgr.wq[iq]))
+            log.info("\t ...")
+        else:
+            for iq in range(qgr.nq):
+                log.info("\t wq[" + str(iq+1) + "]: " + str(qgr.wq[iq]))
+        log.info("\n")
+        log.info("\t " + p.sep)
+        log.info("\n")
+    mpi.comm.Barrier()
+    #
+    # extract phonon data
+    #
+    ph = PhononsClass()
+    ph.set_ph_data(qgr)
+    #
+    # set spin-phonon matrix
+    # in eV/ang units
+    if mpi.rank == mpi.root:
+        log.info("\n")
+        log.info("\t " + p.sep)
+        log.info("\t START SPIN-PHONON COUPLING CALCULATION")
+    sp_ph_inter.compute_spin_ph_coupl(nat, Hsp, ph, qgr, interact_dict)
+    if mpi.rank == mpi.root:
+        log.info("\n")
+        log.info("\t END SPIN-PHONON COUPLING CALCULATION")
+        log.info("\t " + p.sep)
+    exit()
+    #
+    # compute ZFS fluctuations
+    if mpi.rank == mpi.root:
+        log.info("\n")
+        log.info("\t " + p.sep)
+        log.info("\t START ENERGY FLUCTUATIONS CALC.")
+    ZFS_fluct = ZFS_ph_fluctuations()
+    ZFS_fluct.compute_fluctuations(wq, qpts, nat, wu, u)
+    ZFS_fluct.collect_acf_from_processes()
+    if mpi.rank == mpi.root:
+        log.info("\n")
+        log.info("\t ENERGY FLUCTUATIONS CALC. CONCLUDED")
+        log.info("\t " + p.sep)
+    #
+    # if w_resolved define freq. grid
+    if p.w_resolved:
+        p.set_w_grid(wu)
+    Fax = sp_ph_inter.Fzfs_ax
+    print(max(Fax))
+    import sys
+    sys.exit()
+    # 2nd order calculation
+    if p.order_2_correct:
+        # F_axby = <1|S Grad_ax,by D S|1> - <0|S Grad_ax,by D S|0>
+        # F should be in eV/ang^2 units
+        # TODO : uncomment here 
+        #'''
+        sp_ph_inter.set_Faxby_zfs(grad2ZFS, Hsp)
+        Faxby = sp_ph_inter.Fzfs_axby
+        print(np.max(Faxby))
+        #'''
+        Faxby = np.zeros((3*nat, 3*nat), dtype=np.complex128)
+        # eV / ang^2
+    else:
+        Faxby = None
+    # set q pts. grid
+    if p.ph_resolved:
+        p.set_wql_grid(wu, nq, nat)
+    #
+    # compute acf over local (q,l) list
+    acf = acf_sp_ph().generate_instance()
+    acf.allocate_acf_arrays(nat)
+    acf.compute_acf(wq, wu, u, qpts, nat, Fax, Faxby, Hsp)
+    #
+    # collect data from processes
+    acf.collect_acf_from_processes(nat)
+    # test acf -> check t=0 / w=0
+    if log.level <= logging.INFO:
+        acf.auto_correl_test()
+    #
+    # print average atom displ
+    if log.level <= logging.DEBUG:
+        #
+        ph_ampl = PhononAmplitude(nat)
+        # run over q pts
+        #
+        iq_list = mpi.split_list(range(nq))
+        for iq in iq_list:
+            # w(q) data (THz)
+            wuq = wu[iq]
+            # eu(q) data
+            euq = u[iq]
+            # q vector
+            qv = qpts[iq]
+            # compute atoms displ. first
+            ph_ampl.compute_ph_amplq(euq, wuq, nat)
+            ph_ampl.update_atom_displ(wq[iq], qv, wuq, nat)
+            #
+        if mpi.size > 1:
+            ph_ampl.collect_displ_between_proc(nat)
+        # save data on file
+        ph_ampl.print_atom_displ(p.write_dir)
+    mpi.comm.Barrier()
+    # set up T2 calculation
+    T2_calc_handler = set_T2_calc_handler()
+    # prepare data arrays
+    T2_calc_handler.set_up_param_objects_from_scratch(nat)
+    #
+    # compute T2_inv + other phys. parameters
+    T2_calc_handler.extract_physical_quantities(acf, nat)
+    # wait all processes
+    mpi.comm.Barrier()
+    return T2_calc_handler
