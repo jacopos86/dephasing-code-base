3f920de0a5d8e704b5acee2ed00ba5bf0e3f04b3
Jacopo Simoni <simonij@tcd.ie>
2025-04-26 14:11:31 -0500

    create -> build_gs_spin_struct

--- pydephasing/build_unpert_struct.py
+++ pydephasing/build_unpert_struct.py
@@ -1,4 +1,5 @@
 from pydephasing.set_structs import UnpertStruct
+from pydephasing.set_param_object import p
 
 #
 #   This function returns the
@@ -11,4 +12,13 @@ def build_gs_struct_base(gs_data_dir):
     struct0 = UnpertStruct(gs_data_dir)
     # read POSCAR
     struct0.read_poscar()
+    return struct0
+
+def build_gs_spin_struct(gs_data_dir, HFI_CALC):
+    struct0 = build_gs_struct_base(gs_data_dir)
+    # read ZFS tensor
+    struct0.read_zfs_tensor()
+    # check here the transformation
+    if HFI_CALC:
+        struct0.set_hfi_Dbasis(p.fc_core)
     return struct0
