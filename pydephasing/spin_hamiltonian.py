fix spin hamiltonian eigenvectors calculation

pydephasing/spin_hamiltonian.py
@@ -6,7 +6,7 @@ import numpy as np
 from numpy import linalg as LA
 import yaml
 from common.special_functions import delta, triplet_evolution
-from common.phys_constants import hbar, mu_B, eps, THz_to_ev
+from common.phys_constants import hbar, gamma_e, eps, THz_to_ev
 from pydephasing.log import log
 from pydephasing.mpi import mpi
 import logging
@@ -37,7 +37,8 @@ def set_spin_hamiltonian(struct0, B0, nuclear_config=None):
 
 class spin_hamiltonian(ABC):
 	def __init__(self):
-		pass
+		# unperturbed Hamiltonian
+		self.H0 = None
 	def Splus_mtxel(self, j1, m1, j2, m2):
 		r = np.sqrt((j2 - m2) * (j2 + m2 + 1)) * delta(j1, j2) * delta(m1, m2+1)
 		return r
@@ -112,13 +113,19 @@ class spin_triplet_hamiltonian(spin_hamiltonian):
 		self.Sz[2,2] = -1.
 	#   SDS
 	def set_SDS(self, unprt_struct):
-		Ddiag = unprt_struct.Ddiag*2.*np.pi*1.E-6
+		Ddiag = unprt_struct.Ddiag*1.E-6
 		#
 		#  THz units
 		#
 		self.SDS = Ddiag[0]*np.matmul(self.Sx, self.Sx)
 		self.SDS = self.SDS + Ddiag[1]*np.matmul(self.Sy, self.Sy)
 		self.SDS = self.SDS + Ddiag[2]*np.matmul(self.Sz, self.Sz)
+		self.SDS = self.SDS * THz_to_ev
+		# eV units
+		# eigenv.
+		eig = LA.eig(self.SDS)[0]
+		eig0 = LA.eig(self.H0)[0]
+		np.testing.assert_almost_equal(eig, eig0, decimal=5)
 	# hyperfine interaction
 	def hperfine_coupl(self, site, I, Ahfi):
 		# hyperfine coupl. in eV from MHz
@@ -137,7 +144,10 @@ class spin_triplet_hamiltonian(spin_hamiltonian):
 		# unperturbed H0 = D[Sz^2 - s(s+1)/3] + E(Sx^2 - Sy^2) - mu_B B Sz
 		H0 = D * (np.matmul(self.Sz, self.Sz) - self.Ssq / 3.) 
 		H0 +=E * (np.matmul(self.Sx, self.Sx) - np.matmul(self.Sy, self.Sy))
-		H0 -=mu_B * 2.0 * (B[0] * self.Sx + B[1] * self.Sy + B[2] * self.Sz)
+		# store unperturbed Hamiltonian
+		self.H0 = np.copy(H0)
+		# add B field + HFI if present
+		H0 -=gamma_e * (B[0] * self.Sx + B[1] * self.Sy + B[2] * self.Sz)
 		if nuclear_config is not None:
 			for isp in range(nuclear_config.nsp):
 				I = nuclear_config.nuclear_spins[isp]['I']
@@ -147,6 +157,21 @@ class spin_triplet_hamiltonian(spin_hamiltonian):
 		eig, eigv = LA.eig(H0)
 		for s in range(3):
 			self.qs.append({'eig':eig[s], 'eigv':eigv[:,s]})
+		# check eigenvalues
+		self.set_SDS(unprt_struct)
+	# set Hamiltonian time t
+	def set_hamilt_oft(self, t, unprt_struct, B, nuclear_config=None):
+		# get unpert. Hamiltonian
+		H = self.H0
+		# add ext. magnetic field
+		H -=gamma_e * (B[0] * self.Sx + B[1] * self.Sy + B[2] * self.Sz)
+		# hyperfine interaction
+		if nuclear_config is not None:
+			for isp in range(nuclear_config.nsp):
+				It = nuclear_config.nuclear_spins[isp]['I'][t]
+				site = nuclear_config.nuclear_spins[isp]['site']
+				H += self.hperfine_coupl(site, It, unprt_struct.Ahfi)
+		return H
 	# set time array
 	def set_time(self, dt, T):
 		# set time in ps units