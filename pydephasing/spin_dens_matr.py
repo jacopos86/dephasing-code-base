ac69af9ab06630c869df5081a5ba360a1fba9928
jacopos86 <simonij@tcd.ie>
2025-04-28 10:35:13 -0500

    initialization spin DM

--- pydephasing/spin_dens_matr.py
+++ pydephasing/spin_dens_matr.py
@@ -1,3 +1,7 @@
+import numpy as np
+from pydephasing.log import log
+from pydephasing.mpi import mpi
+from pydephasing.set_param_object import p
 
 #
 # spin density matrix class -> rho_{a,b}
@@ -7,3 +11,11 @@ class spin_dmatr(object):
     def __init__(self):
         # hilbert space dim.
         self.n = None
+        self.matr0 = None
+    def initialize(self, psi0):
+        self.n = len(psi0)
+        if mpi.rank == mpi.root:
+            log.info("\t spin space dimension: " + str(self.n))
+            log.info("\t " + p.sep)
+        self.matr0 = np.kron(psi0, psi0.conj()).reshape((self.n, self.n))
+        np.testing.assert_almost_equal(self.matr0.trace().real, 1.0)
