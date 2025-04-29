f3847788e24211d793824451d9332128b7e41c52
jacopos86 <simonij@tcd.ie>
2025-04-29 09:57:48 -0500

    compute_occup_prob -> added

--- pydephasing/observables.py
+++ pydephasing/observables.py
@@ -3,8 +3,11 @@ import numpy as np
 #  This modules computes different observables
 #  from the system density matrix
 
-# compute spin magnetization
-# expect. value
+#
+#    compute spin magnetization
+#    expect. value
+#
+
 def compute_spin_mag(H, dm_obj):
     # compute magnet. expect. value
     rho = dm_obj.rho
@@ -19,11 +22,20 @@ def compute_spin_mag(H, dm_obj):
         M_oft[0,t] = np.matmul(rho[:,:,t], sx).trace().real
         M_oft[1,t] = np.matmul(rho[:,:,t], sy).trace().real
         M_oft[2,t] = np.matmul(rho[:,:,t], sz).trace().real
-    # plot spin magnetization
-    if log.level <= logging.DEBUG:
-        plt.ylim([-1.5, 1.5])
-        plt.plot(self.time, self.Mt[0,:].real, label="X")
-        plt.plot(self.time, self.Mt[1,:].real, label="Y")
-        plt.plot(self.time, self.Mt[2,:].real, label="Z")
-        plt.legend()
-        plt.show()
+    return M_oft
+
+#  
+#   compute occupations probability
+#
+
+def compute_occup_prob(dm_obj):
+    # compute occup. prob.
+    rho = dm_obj.rho
+    nt = rho.shape[-1]
+    n = rho.shape[0]
+    occup = np.zeros((n, nt))
+    # run over time array
+    for t in range(nt):
+        for i in range(n):
+            occup[i,t] = (rho[i,i,t] * rho[i,i,t].conj()).real
+    return occup
