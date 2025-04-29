5a7497aaa48691e1c4a405fbb188dd70631dbb10
Jacopo Simoni <simonij@tcd.ie>
2025-04-29 07:02:32 -0500

    add -> compute_spin_vector

--- pydephasing/common/real_time_module.py
+++ pydephasing/common/real_time_module.py
@@ -31,3 +31,27 @@ def ODE_solver(y0, F, dt):
         #
         yt[:,i+1] = y[:] + (K1[:] + 2.*K2[:] + 2.*K3[:] + K4[:]) / 6.
     return yt
+
+# compute spin vector
+def compute_spin_vector_evol(self, struct0, psi0, B):
+		# initial state : psi0
+		# magnetic field : B (gauss)
+		# H = SDS + gamma_e B S
+		# 1) compute SDS
+		self.set_SDS(struct0)
+		# n. time steps
+		nt = len(self.time_dense)
+		Ht = np.zeros((3,3,nt), dtype=np.complex128)
+		# 2) set Ht in eV units
+		# run over time steps
+		for i in range(nt):
+			Ht[:,:,i] = Ht[:,:,i] + hbar * self.SDS[:,:]
+			# eV
+			# add B field
+			Ht[:,:,i] = Ht[:,:,i] + gamma_e * hbar * (B[0] * self.Sx[:,:] + B[1] * self.Sy[:,:] + B[2] * self.Sz[:,:])
+		dt = self.time[1]-self.time[0]
+		# ps units
+		# triplet wave function evolution
+		self.tripl_psit = triplet_evolution(Ht, psi0, dt)
+		# set magnetization vector Mt
+		self.set_magnetization()
