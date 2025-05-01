e043fbea4400d7f1b08084e25f6b5107834125f8
Jacopo Simoni <simonij@tcd.ie>
2025-05-01 08:41:48 -0500

    create time arrays

--- pydephasing/real_time_solver_base.py
+++ pydephasing/real_time_solver_base.py
@@ -11,3 +11,14 @@ class RealTimeSolver(ABC):
         self.dt = None
         # total simulation time
         self.T = None
+        # n. time steps
+        self.nt = None
+    # set time arrays
+    def set_time_arrays(self, dt, T):
+        # n. time steps
+        self.nt = int(T / dt)
+        self.time = np.linspace(0., T, self.nt)
+        # build dense array
+        nt2 = int(T / (dt/2.))
+        self.time_dense = np.linspace(0., T, nt2)
+        # everything must be in fs
