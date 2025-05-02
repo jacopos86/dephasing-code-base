f24d7c5b70d4dcfb08a49d080b32a943aefdb14f
jacopos86 <simonij@tcd.ie>
2025-05-02 08:57:37 -0500

    reorganize the setting of real time solver

--- pydephasing/set_real_time_solver.py
+++ pydephasing/set_real_time_solver.py
@@ -10,36 +10,29 @@ from pydephasing.Liouville_solver import LiouvilleSolver
 
 def set_real_time_solver():
     calc_type1 = parser.parse_args().ct1[0]
-    # no real time solver
-    if calc_type1 == "LR":
-        return None
-    elif calc_type1 == "LBLD":
+    # assert calc_type1 is set to RT
+    assert calc_type1 == "RT"
+    if mpi.rank == mpi.root:
+        log.info("\n")
+        log.info("\t " + p.sep)
+        log.info("\t SETTING UP REAL TIME SOLVER")
+        log.info("\t " + p.sep)
+        log.info("\n")
+    # read dynamical mode
+    # dynamical[i] -> i=0 : one-phonon process - i=1 : two-phonon process 
+    if p.dynamical_mode[0] == 0 and p.dynamical_mode[1] == 0:
         if mpi.rank == mpi.root:
-            log.info("\n")
-            log.info("\t " + p.sep)
-            log.info("\t SETTING LINDBLAD SOLVER")
+            log.info("\t FULLY COHERENT EVOLUTION ")
             log.info("\t " + p.sep)
             log.info("\n")
-        return None
-    elif calc_type1 == "NMARK":
+        return LiouvilleSolver()
+    elif p.dynamical_mode[0] == 1 and p.dynamical_mode[1] == 0:
         if mpi.rank == mpi.root:
+            log.info("\t LINDBLAD SOLVER - ELEC-PH ORDER 1")
+            log.info("\t " + p.sep)
             log.info("\n")
+    elif p.dynamical_mode[0] == 2 and p.dynamical_mode[1] == 0:
+        if mpi.rank == mpi.root:
+            log.info("\t COMPLETE NON MARKOVIAN SOLVER - ELEC-PH ORDER 1")
             log.info("\t " + p.sep)
-            log.info("\t SETTING NON MARKOVIAN SOLVER")
-            log.info("\t " + p.sep)
-        if p.dynamical_mode[0] == 0 and p.dynamical_mode[1] == 0:
-            if mpi.rank == mpi.root:
-                log.info("\t FULLY COHERENT EVOLUTION ")
-                log.info("\t " + p.sep)
-                log.info("\n")
-            return LiouvilleSolver()
-        elif p.dynamical_mode[0] == 1 and p.dynamical_mode[1] == 0:
-            if mpi.rank == mpi.root:
-                log.info("\t LINDBLAD SOLVER - ELEC-PH ORDER 1")
-                log.info("\t " + p.sep)
-                log.info("\n")
-        elif p.dynamical_mode[0] == 2 and p.dynamical_mode[1] == 0:
-            if mpi.rank == mpi.root:
-                log.info("\t NON MARKOVIAN SOLVER - ELEC-PH ORDER 1")
-                log.info("\t " + p.sep)
-                log.info("\n")
+            log.info("\n")
