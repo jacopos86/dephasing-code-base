e6a964f5d02505ac078daeff0696ae9a15e14a34
jacopos86 <simonij@tcd.ie>
2025-05-03 10:52:05 -0500

    magnetic field -> create mode

---
+++ pydephasing/magnetic_field.py
@@ -0,0 +1,15 @@
+
+#
+#  magnetic field class
+#
+
+class magnetic_field(object):
+    def __init__(self):
+        # amplitude
+        self.B0 = None
+    def set_pulse(self, time):
+        nt = len(time)
+        for t in range(nt):
+            pass
+    def set_pulse_sequence(self):
+        pass
