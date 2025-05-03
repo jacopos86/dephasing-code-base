f05d3841c736115543d9587d0a2d2645035237aa
Jacopo Simoni <simonij@tcd.ie>
2025-05-03 08:56:16 -0500

    include propagate

--- pydephasing/Liouville_solver.py
+++ pydephasing/Liouville_solver.py
@@ -1,4 +1,10 @@
 from pydephasing.real_time_solver_base import RealTimeSolver
+from pydephasing.log import log
+from pydephasing.mpi import mpi
+from pydephasing.set_param_object import p
+from common.phys_constants import hbar
+from common.matrix_operations import commute
+import numpy as np
 
 #
 #    This module implements
@@ -12,3 +18,33 @@ class LiouvilleSolver(RealTimeSolver):
     def evolve(self, dt, T, rho, H):
         # set time arrays
         self.set_time_arrays(dt, T)
+        # dynamics purely
+        # drho/dt = -i[H, rho]
+        # propagate DM
+        self.propagate(rho, H)
+        # compute observables
+    # time propagation DM
+    # drho/dt = i[H,\rho]/\hbar
+    def propagate(self, rho, H):
+        # initialize DM
+        rh0 = rho.matr0
+        n = rh0.shape[0]
+        if mpi.rank == mpi.root:
+            log.info("\n")
+            log.info("\t " + p.sep)
+            log.info("\t STARTING REAL TIME EVOLUTION")
+            log.info("\t nt: " + str(self.nt))
+            log.info("\t dt (ps): " + str(self.dt))
+            log.info("\t T (ps): " + str(self.T))
+            log.info("\t " + p.sep)
+        rh = np.zeros((n, n, self.nt), dtype=np.complex128)
+        rh[:,:,0] = rh0[:,:]
+        # iterate over time step
+        for t in range(int(self.nt/2)-1):
+            y = np.zeros((n,n), dtype=np.complex128)
+            y[:,:] = rh[:,:,t]
+            # F1 (ps^-1)
+            h = H.set_hamilt_oft(2*self.time_dense[2*t], unprt_struct, B[2*t], nuclear_config)
+            F1 = 1j * commute(h, y) / hbar
+            K1 = self.dt * F1
+            y1 = y + K1 / 2.0
