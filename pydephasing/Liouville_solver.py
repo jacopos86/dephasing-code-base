c8e428b231821dd6bff04a8511ffd07e97c64c52
Jacopo Simoni <simonij@tcd.ie>
2025-05-31 20:32:53 -0500

    build propagate function for Liouville evolution

--- pydephasing/Liouville_solver.py
+++ pydephasing/Liouville_solver.py
@@ -7,44 +7,48 @@ from common.matrix_operations import commute
 import numpy as np
 
 #
-#    This module implements
-#    the Liouville solver for DM dynamics
+#      Liouville solver without nuclear spins
 #
 
 class LiouvilleSolver(RealTimeSolver):
     def __init__(self):
-        super(LiouvilleSolver, self).__init__()
+        super().__init__(LiouvilleSolver, self).__init__()
     # evolve DM
-    def evolve(self, dt, T, rho, H):
+    def evolve(self, dt, T, rho, H, Bfield):
         # set time arrays
-        self.set_time_arrays(dt, T)
+        time = self.set_time_array(dt/2., T)
+        # compute ext. magnetic field
+        B = Bfield.of_t(time)
         # dynamics purely
         # drho/dt = -i[H, rho]
         # propagate DM
-        self.propagate(rho, H)
+        self.propagate(rho, H, B, dt, T)
         # compute observables
-    # time propagation DM
-    # drho/dt = i[H,\rho]/\hbar
-    def propagate(self, rho, H):
+    #
+    #  set time propagation function
+    def propagate(self, rho, H, B, dt, T):
         # initialize DM
         rh0 = rho.matr0
         n = rh0.shape[0]
+        # time variables
+        time = self.set_time_array(dt, T)
+        nt = len(time)
         if mpi.rank == mpi.root:
             log.info("\n")
             log.info("\t " + p.sep)
             log.info("\t STARTING REAL TIME EVOLUTION")
-            log.info("\t nt: " + str(self.nt))
-            log.info("\t dt (ps): " + str(self.dt))
-            log.info("\t T (ps): " + str(self.T))
+            log.info("\t nt: " + str(nt))
+            log.info("\t dt (ps): " + str(dt))
+            log.info("\t T (ps): " + str(T))
             log.info("\t " + p.sep)
-        rh = np.zeros((n, n, self.nt), dtype=np.complex128)
+        rh = np.zeros((n, n, nt), dtype=np.complex128)
         rh[:,:,0] = rh0[:,:]
         # iterate over time step
-        for t in range(int(self.nt/2)-1):
+        for t in range(nt):
             y = np.zeros((n,n), dtype=np.complex128)
             y[:,:] = rh[:,:,t]
             # F1 (ps^-1)
-            h = H.set_hamilt_oft(2*self.time_dense[2*t], unprt_struct, B[2*t], nuclear_config)
+            h = H.set_hamilt_oft(2*t, B)
             F1 = 1j * commute(h, y) / hbar
             K1 = self.dt * F1
             y1 = y + K1 / 2.0
