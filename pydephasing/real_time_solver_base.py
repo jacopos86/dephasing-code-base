68930a500f1dde481b7cfe19b0728f624c2e12c2
Jacopo Simoni <simonij@tcd.ie>
2025-05-31 20:33:48 -0500

    changed set_time_array -> pass dt, T

--- pydephasing/real_time_solver_base.py
+++ pydephasing/real_time_solver_base.py
@@ -8,20 +8,16 @@ import numpy as np
 
 class RealTimeSolver(ABC):
     def __init__(self):
-        # time interval
-        self.dt = None
-        # total simulation time
-        self.T = None
-        # n. time steps
-        self.nt = None
+        pass
     # set time arrays
-    def set_time_arrays(self, dt, T):
+    def set_time_array(self, dt, T):
         # n. time steps
-        self.nt = int(T / dt)
-        self.time = np.linspace(0., T, self.nt)
+        nt = int(T / dt)
+        time = np.linspace(0., T, nt)
         # build dense array
-        nt2 = int(T / (dt/2.))
-        self.time_dense = np.linspace(0., T, nt2)
+        #nt2 = int(T / (dt/2.))
+        #self.time_dense = np.linspace(0., T, nt2)
         # everything must be in ps
-        self.T = T
-        self.dt = dt
+        #self.T = T
+        #self.dt = dt
+        return time
