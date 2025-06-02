d9908204a5837f98b6a89ac7ed0bd17c62fed59f
jacopos86 <simonij@tcd.ie>
2025-06-02 10:23:53 -0500

    Liouville solver -> implemented time dynamics

--- pydephasing/Liouville_solver.py
+++ pydephasing/Liouville_solver.py
@@ -19,11 +19,16 @@ class LiouvilleSolverSpin(RealTimeSolver):
         # set time arrays
         time = self.set_time_array(dt/2., T)
         # compute ext. magnetic field
-        B = Bfield.of_t(time)
+        B = Bfield.set_pulse_oft(time)
+        import matplotlib.pyplot as plt
+        plt.plot(time[:], B[2,:])
+        plt.show()
         # dynamics purely
         # drho/dt = -i[H, rho]
         # propagate DM
-        self.propagate(rho, H, B, dt, T)
+        time, rh = self.propagate(rho, H, B, dt, T)
+        rho.set_density_matr(time, rh)
+        rho.trace_oft()
         # compute observables
     #
     #  set time propagation function
@@ -45,11 +50,37 @@ class LiouvilleSolverSpin(RealTimeSolver):
         rh = np.zeros((n, n, nt), dtype=np.complex128)
         rh[:,:,0] = rh0[:,:]
         # iterate over time step
-        for t in range(nt):
+        for t in range(nt-1):
             y = np.zeros((n,n), dtype=np.complex128)
             y[:,:] = rh[:,:,t]
             # F1 (ps^-1)
             h = H.set_hamilt_oft(2*t, B)
             F1 = 1j * commute(h, y) / hbar
-            K1 = self.dt * F1
+            K1 = dt * F1
             y1 = y + K1 / 2.0
+            # F2
+            h = H.set_hamilt_oft(2*t+1, B)
+            F2 = 1j * commute(h, y1) / hbar
+            K2 = dt * F2
+            y2 = y + K2 / 2.0
+            # F3
+            F3 = 1j * commute(h, y2) / hbar
+            K3 = dt * F3
+            y3 = y + K3
+            # F4
+            h = H.set_hamilt_oft(2*t+2, B)
+            F4 = 1j * commute(h, y3) / hbar
+            K4 = dt * F4
+            rh[:,:,t+1] = y + (K1 + 2.*K2 + 2.*K3 + K4) / 6.0
+        return time, rh
+
+#
+#       Liouville solver with nuclear spins
+#
+
+class LiouvilleSolverHFI(RealTimeSolver):
+    def __init__(self):
+        super(LiouvilleSolverHFI, self).__init__()
+    # evolve DM
+    def evolve(self, dt, T, rho, H, Bfield):
+        pass
