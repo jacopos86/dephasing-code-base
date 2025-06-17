30c914a5054e47c7c3e3b8b31cc09f46ec6c447c
jacopos86 <simonij@tcd.ie>
2025-06-17 09:23:50 -0500

    added -> compute_oneph_decoher

---
+++ pydephasing/gpu_source/compute_oneph_decoher.cu
@@ -0,0 +1,30 @@
+#include <pycuda-complex.hpp>
+#include <cuComplex.h>
+typedef pycuda::complex<double> cmplx;
+
+__global__ void compute_T1_oneph_w_resolved() {
+    /* internal variables */
+    const int i = threadIdx.x + blockDim.x * blockIdx.x;
+    const int j = threadIdx.y + blockDim.y * blockIdx.y;
+    const int k = threadIdx.z + blockDim.z * blockIdx.z;
+    const int idx = i + j * blockDim.x * gridDim.x + k * blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+
+    /* local variables */
+
+
+
+}
+
+
+__global__ void compute_T1_oneph_time_resolved(int n, int *INIT_INDEX, int *SIZE_LIST) {
+    /* internal variables */
+    const int i = threadIdx.x + blockDim.x * blockIdx.x;
+    const int j = threadIdx.y + blockDim.y * blockIdx.y;
+    const int k = threadIdx.z + blockDim.z * blockIdx.z;
+    const int idx = i + j * blockDim.x * gridDim.x + k * blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+
+    /* local variables */
+
+
+
+}
