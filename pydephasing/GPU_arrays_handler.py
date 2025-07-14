6b4ba9b2454acfed4bf730de0c242990a1501ea1
jacopos86 <simonij@tcd.ie>
2025-07-14 10:15:48 -0500

    remove reshape array

--- pydephasing/GPU_arrays_handler.py
+++ pydephasing/GPU_arrays_handler.py
@@ -14,13 +14,6 @@ class GPU_ARRAY:
         return self.cpu_array.astype(self.type)
     def length(self):
         return np.int32(len(self.cpu_array))
-    def reshape_gpu_array(self, gpu_array, index_list, shape):
-        print('shape: ', shape)
-        print('index shape: ', index_list.shape, len(index_list))
-        print('gpu array shape', gpu_array.shape)
-        array = np.zeros(shape)
-        print(array.shape)
-        return array
     def print_array(self):
         print(self.shape)
         if len(self.shape) == 1:
