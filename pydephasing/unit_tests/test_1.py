8f061f935d89d5be1ffdf7ca73ceb08a0313dca6
jacopos86 <simonij@tcd.ie>
2025-07-30 02:21:21 -0500

    create test_1

---
+++ pydephasing/unit_tests/test_1.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+from pydephasing.log import log
+from pydephasing.input_parameters import preproc_data_input
+
+def parse_args(sys_args):
+    parse = argparse.ArgumentParser(description="Command line program.")
+    parse.add_argument("--yml_inp", type=str,
+                       help="Enter input")
+    return parse.parse_args(sys_args)
+
+def read_parameters(yml_inp):
+    pobj = preproc_data_input()
+    # read data
+    pobj.read_yml_data(yml_inp)
+    return pobj
+
+def run(pobj):
+    # start test
+    log.info("\n")
+    log.info("\t " + pobj.sep)
+    log.info("\t BUILD DISPLACED STRUCTS.")
+
+def test_program_parse():
+    TESTS_DIR = os.environ['TESTS']
+    yml_inp = str(TESTS_DIR) + "/1/input.yml"
+    argv_list = ["--yml_inp", yml_inp]
+    args = parse_args(argv_list)
+    yml_inp_parse = str(TESTS_DIR) + "/1/input.yml"
+    assert(yml_inp == yml_inp_parse)
+
+def test_parameters():
+    TESTS_DIR = os.environ['TESTS']
+    yml_inp = str(TESTS_DIR) + "/1/input.yml"
+    pobj = read_parameters(yml_inp)
+    assert(pobj.unpert_dir, str(TESTS_DIR)+"/1/GS")
+    assert(pobj.displ_poscar_dir[0], str(TESTS_DIR)+"/1/DISPLACEMENT-FILES-001")
+    assert(pobj.displ_outcar_dir[0], str(TESTS_DIR)+"/1/DISPL-001")
+    assert(pobj.atoms_displ[0][0], 0.01)
+    assert(pobj.atoms_displ[0][1], 0.01)
+    assert(pobj.atoms_displ[0][2], 0.01)
+    assert(pobj.defect_index, 0)
+    assert(pobj.max_dab, 2.7)
