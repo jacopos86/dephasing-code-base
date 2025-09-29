cae62d433d931e1061be4b4ff2d5c5b52362b778
jacopos86 <simonij@tcd.ie>
2025-09-29 10:16:18 -0500

    create bell circuit test

--- pydephasing/unit_tests/test_2.py
+++ pydephasing/unit_tests/test_2.py
@@ -24,3 +24,21 @@ def test_hadamard():
     p1 = counts['1'] / 2000
     assert p0 == pytest.approx(0.5, abs=0.05)
     assert p1 == pytest.approx(0.5, abs=0.05)
+
+def test_bell_circuit():
+    # Bell circuit
+    qc = QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure([0, 1], [0, 1])
+    assert qc.width() == 4
+    assert qc.size() == 4
+    # run circuit
+    backend = AerSimulator()
+    job = backend.run(qc, shots=2000)
+    result = job.result()
+    counts = result.get_counts()
+    # assert results
+    assert '00' in counts
+    assert '11' in counts
+    assert counts['00'] + counts['11'] > 900  # > 90% fidelity
