44291d0fdd2e10322f1589820364fa253047ca27
Jacopo Simoni <simonij@tcd.ie>
2025-09-30 23:33:48 -0500

    set PauliPolynomials

--- pydephasing/quantum/pauli_polynomial_class.py
+++ pydephasing/quantum/pauli_polynomial_class.py
@@ -3,5 +3,19 @@
 #
 
 class PauliPolynomial:
-    def __init__(self, nq):
-        self.nq = nq
+    def __init__(self, pol=None):
+        self.__pol = []
+        if pol is not None:
+            self.__pol.extend(pol)
+            self._reduce()
+    def return_polynomial(self):
+        return self.__pol
+    def count_number_terms(self):
+        return len(self.__pol)
+    def __add__(self, pp):
+        if not isinstance(pp, PauliPolynomial):
+            return NotImplemented
+        new_pol = self.__pol + pp.return_polynomial()
+        return PauliPolynomial(new_pol)
+    def _reduce(self):
+        pass
