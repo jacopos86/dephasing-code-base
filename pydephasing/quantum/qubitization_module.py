68cfa54b422fe5f221a25d8039be467cd1382c45
Jacopo Simoni <simonij@tcd.ie>
2025-10-09 09:27:50 -0500

    set pw2strng + pauli term initialization

--- pydephasing/quantum/qubitization_module.py
+++ pydephasing/quantum/qubitization_module.py
@@ -1,5 +1,4 @@
 from utilities.log import log
-from quantum.pauli_polynomial_class import PauliPolynomial
 from quantum.pauli_letters_module import PauliLetter
 from set_param_object import p
 
@@ -25,85 +24,38 @@ class PauliTerm:
         else:
             log.error("WRONG INITIALIZATION")
     def __set_pauli_term(self, ps, pc):
-        p_seq = []
+        pl_seq = []
         for iq in range(self.__nq):
-            pl = PauliLetter(ps[iq])
-            p_seq.append(pl)
-        self.pw = p_seq
+            pl = PauliLetter(symbol=ps[iq])
+            pl_seq.append(pl)
+        self.pw = pl_seq
         self.p_coeff = pc
-        print(self.pw)
     def __set_pauli_term_from_letters(self, pl_seq, p_coeff):
         self.p_coeff = p_coeff
-        self.pw = ""
+        self.pw = []
         for pl in pl_seq:
             self.p_coeff *= pl.phase
-            self.pw += pl.symbol
+            pl.phase = 1.
+            self.pw.append(pl)
+    def nqubit(self):
+        return self.__nq
     def __mul__(self, pt):
         if not isinstance(pt, PauliTerm):
             return NotImplemented
         pl_seq = []
         for iq in range(self.__nq):
             r = self.pw[iq]*pt.pw[iq]
-            print(self.pw[iq].symbol, pt.pw[iq].symbol, r.symbol)
             pl_seq.append(r)
         return PauliTerm(self.__nq, pc=self.p_coeff*pt.p_coeff, pl_seq=pl_seq)
+    def pw2strng(self):
+        strng = ""
+        for iq in range(self.__nq):
+            strng += self.pw[iq].symbol
+        return strng
     def visualize(self):
-        strng = "\t " + str(self.p_coeff) + self.pw
+        strng = "\t " + str(self.p_coeff) + " "
+        for iq in range(self.__nq):
+            strng += self.pw[iq].symbol
         log.info("\t " + p.sep)
         log.info(strng)
         log.info("\t " + p.sep)
-#
-#   Define cj^+ fermionic operator
-#   in qubit representation
-
-class fermion_plus_operator(PauliPolynomial):
-    def __init__(self, repr_mode, nq, j):
-        super().__init__(repr_mode)
-        if j < 0 or j >= nq:
-            log.error("index j out of range -> 0 <= j < nq")
-        if self._repr_mode == "JW":
-            self.__set_JW_operator(nq, j)
-    def __set_JW_operator(self, nq, j):
-        strng = ""
-        for _ in range(nq, j+1, -1):
-            strng += "e"
-        strng += "x"
-        for _ in range(j, 0, -1):
-            strng += "z"
-        self._pol.append(PauliTerm(nq, ps=strng, pc=0.5))
-        strng = ""
-        for _ in range(nq, j+1, -1):
-            strng += "e"
-        strng += "y"
-        for _ in range(j, 0, -1):
-            strng += "z"
-        self._pol.append(PauliTerm(nq, ps=strng, pc=-0.5j))
-        self._reduce()
-
-#
-#   Define cj fermionic operator
-#   in qubit representation
-
-class fermion_minus_operator(PauliPolynomial):
-    def __init__(self, repr_mode, nq, j):
-        super().__init__(repr_mode)
-        if j < 0 or j >= nq:
-            log.error("index j out of range -> 0 <= j < nq")
-        if self._repr_mode == "JW":
-            self.__set_JW_operator(nq, j)
-    def __set_JW_operator(self, nq, j):
-        strng = ""
-        for _ in range(nq, j+1, -1):
-            strng += "e"
-        strng += "x"
-        for _ in range(j, 0, -1):
-            strng += "z"
-        self._pol.append(PauliTerm(nq, ps=strng, pc=0.5))
-        strng = ""
-        for _ in range(nq, j+1, -1):
-            strng += "e"
-        strng += "y"
-        for _ in range(j, 0, -1):
-            strng += "z"
-        self._pol.append(PauliTerm(nq, ps=strng, pc=0.5j))
-        self._reduce()
