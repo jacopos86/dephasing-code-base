2a6016ec1c55f7c0548e3043f580fa33961077a8
Jacopo Simoni <simonij@tcd.ie>
2025-10-16 16:35:55 -0500

    electron phonon inter. class

--- pydephasing/elec_ph_inter.py
+++ pydephasing/elec_ph_inter.py
@@ -1,22 +1,20 @@
 import numpy as np
+from abc import ABC
+from parallelization.mpi import mpi
+from utilities.log import log
 #
 #   This module computes the electron-phonon matrix
 #   g_munu(k,q)
 #   read external data grad H0
 #   and compute the matrix using the phonon modes
 #
-class electron_phonon:
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
-    # read data
-    # from saved file
-    def read_eph_matrix(self):
-        pass
+class ElectronPhononClass(ABC):
+    def __init__(self):
+        self.g_ql = None
     #
     # compute e-ph matrix from
     # grad_X He
-    def compute_eph_matrix(self, nat, ql_list, He):
+    def compute_gql(self, ql_list, He, gHe):
         n = len(He.basis_vectors)
         g_ql = np.zeros((n, n, len(ql_list)), dtype=np.complex128)
         # phonons amplitudes
-        
