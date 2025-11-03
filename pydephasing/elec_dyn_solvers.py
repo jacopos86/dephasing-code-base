0022733d0ecf47fd3b22ee651d3a43389d205408
jacopos86 <simonij@tcd.ie>
2025-11-03 10:10:33 -0600

    create -> elec_dyn_solvers.py

---
+++ pydephasing/elec_dyn_solvers.py
@@ -0,0 +1,44 @@
+# This is the main subroutine
+# for non Markovian calculation of electronic systems
+#
+from pydephasing.set_param_object import p
+from parallelization.mpi import mpi
+from utilities.log import log
+from wannier_interface.wannier import Wannier
+from pydephasing.phonons_module import JDFTxPhonons
+
+def solve_elec_dyn_VASP_data():
+    '''
+    use VASP data to perform RT dynamics
+    '''
+
+
+
+
+
+
+def solve_elec_dyn_JDFTx_data():
+    '''
+    use JDFTx data to perform RT dynamics
+    '''
+    if mpi.rank == mpi.root:
+        log.info("\t " + p.sep)
+        log.info("\n")
+        log.info("\t PERFORM REAL TIME DYNAMICS")
+        log.info("\t COLLECT DATA FROM JDFTx CALCULATION")
+        log.info("\n")
+        log.info("\t " + p.sep)
+    # read Wannier data
+    wan = Wannier(p.eigenv_file, p.bnd_kpts_file)
+    wan.set_wann_transf()
+    if p.dynamical_mode[1] > 0:
+        # read phonons data
+        # if we need to compute e-ph interactions
+        if mpi.rank == mpi.root:
+            log.info("\t " + p.sep)
+            log.info("\t COLLECT PHONONS INFORMATION")
+            log.info("\t " + p.sep)
+            log.info("\n")
+        ph = JDFTxPhonons(p.ph_cellmap_file, p.ph_eigenv_file, p.ph_outfile)
+        ph.read_ph_hamilt()
+        ph.get_ph_supercell()
