4415aecb31c5cd58a82189fffbfdf8a39cb5cae2
jacopos86 <simonij@tcd.ie>
2025-11-30 11:28:43 -0600

    include jdftx phonon module and compute ph OAM

--- pydephasing/phonons_module.py
+++ pydephasing/phonons_module.py
@@ -2,14 +2,19 @@ import h5py
 import logging
 import numpy as np
 from abc import ABC
-from parallelization.mpi import mpi
-from common.phys_constants import eps, kb, hbar
+from pydephasing.parallelization.mpi import mpi
+from pydephasing.common.phys_constants import eps, kb, hbar
 from pydephasing.set_param_object import p
-from utilities.log import log
+from pydephasing.utilities.log import log
+from pydephasing.common.phys_constants import hartree2ev, THz_to_ev
+from pydephasing.utilities.plot_functions import plot_ph_band_struct, plot_lph_struct
+from pydephasing.atomic_list_struct import atoms
 from math import exp
+
 #
 #  phonons class
 #
+
 class PhononsClass(ABC):
     def __init__(self):
         self.eql = None
@@ -130,18 +135,35 @@ class PhonopyPhonons(PhononsClass):
 #
 
 class JDFTxPhonons(PhononsClass):
-    def __init__(self, CELLMAP_FILE, EIGENV_FILE, OUT_FILE):
+    def __init__(self, CELLMAP_FILE, EIGENV_FILE, OUT_FILE, PHBASIS_FILE, TR_SYM=True):
         super().__init__()
+        self.cellMapPh = None
+        self.nCellsPh = None
+        self.phBasis = None
+        self.Hph = None
+        self.ForceMatrix = None
+        self.supercell = None
+        self.lph_eq = None
+        self.Mph = None
+        # input files
         self.CELLMAP_FILE = CELLMAP_FILE
         self.EIGENV_FILE = EIGENV_FILE
         self.OUT_FILE = OUT_FILE
-        self.nCellsPh = None
-        self.Hph = None
-        self.supercell = None
+        self.PHBASIS_FILE = PHBASIS_FILE
+        self.TR_SYM = TR_SYM
     def read_ph_cell_map(self):
-        cellMapPh = np.loadtxt(self.CELLMAP_FILE)[:,:3].astype(int)
-        self.nCellsPh = cellMapPh.shape[0]
-        print(self.nCellsPh)
+        self.cellMapPh = np.loadtxt(self.CELLMAP_FILE)[:,:3].astype(int)
+        self.nCellsPh = self.cellMapPh.shape[0]
+    def read_force_matrix(self):
+        self.read_ph_cell_map()
+        Forces = np.fromfile(self.EIGENV_FILE, dtype=np.float64)
+        # n. modes
+        self.nmodes = int(np.sqrt(Forces.shape[0] / self.nCellsPh))
+        if mpi.rank == mpi.root:
+            log.info("\t " + p.sep)
+            log.info("\t n. ph. modes: " + str(self.nmodes))
+            log.info("\t " + p.sep)
+        self.ForceMatrix = np.reshape(Forces, (self.nCellsPh,self.nmodes,self.nmodes))
     def get_ph_supercell(self):
         for line in open(self.OUT_FILE):
             tokens = line.split()
@@ -151,13 +173,69 @@ class JDFTxPhonons(PhononsClass):
         if mpi.rank == mpi.root:
             log.info("\t ph. supercell grid: " + str(self.supercell))
             log.info("\t " + p.sep)
-    def read_ph_hamilt(self):
-        self.read_ph_cell_map()
-        Hphonon = np.fromfile(self.EIGENV_FILE, dtype=np.float64)
-        self.nmodes = int(np.sqrt(Hphonon.shape[0] / self.nCellsPh))
+    def read_phonon_basis(self):
+        self.phBasis = np.loadtxt(self.PHBASIS_FILE, usecols=[2,3,4])
+        self.phBasis = np.sqrt(np.sum(self.phBasis ** 2, axis=1))
+    def compute_energy_dispersion(self, qgr, n_interp=10):
+        # set q grid
+        qp, n = qgr.set_qgr_plot(n_interp)
         if mpi.rank == mpi.root:
+            log.info("\t COMPUTE PHONON DISPERSION")
             log.info("\t " + p.sep)
-            log.info("\t n. ph. modes: " + str(self.nmodes))
+        TildeForceMatrix = np.tensordot(np.exp((2j*np.pi)*np.dot(qp, self.cellMapPh.T)), self.ForceMatrix, axes=1)
+        # diagonalization
+        omegaSq, normalModes = np.linalg.eigh(TildeForceMatrix)   # energies in Hartree
+        omega = np.copysign(np.sqrt(np.abs(omegaSq)), omegaSq)
+        if mpi.rank == mpi.root:
+            log.info("\t shape wq^2: " + str(omega.shape))
             log.info("\t " + p.sep)
-        self.Hph = np.reshape(Hphonon, (self.nCellsPh,self.nmodes,self.nmodes)).swapaxes(1,2)
-        # read ph. basis
+            plot_ph_band_struct(omega * hartree2ev * 1.E3, n)  # energies in meV
+        mpi.comm.Barrier()
+    def compute_ph_state_q(self, q):
+        phase = np.exp((2j*np.pi)*np.dot(q, self.cellMapPh.T))
+        D = np.tensordot(phase, self.ForceMatrix, axes=1)
+        omegaSq, Wq = np.linalg.eigh(D)
+        omega = np.copysign(np.sqrt(np.abs(omegaSq)), omegaSq)
+        omega_q = omega * hartree2ev / THz_to_ev        # THz
+        return omega_q, Wq
+    def compute_eq_ph_angular_momentum_dispersion(self, qgr, n_interp=10):
+        # set q grid
+        qp, n = qgr.set_qgr_plot(n_interp)
+        self.lph_eq = np.zeros((3,self.nmodes,n), dtype=np.complex128)
+        if mpi.rank == mpi.root:
+            log.info("\t TR SYMMETRY: " + str(self.TR_SYM))
+            log.info("\t " + p.sep)
+        # compute lph at each q
+        wq_list = []
+        for i, q in enumerate(qp):
+            qm = -q
+            omega_q, Wq = self.compute_ph_state_q(q)
+            omega_qm, Wqm = self.compute_ph_state_q(qm)
+            if self.TR_SYM:
+                if not np.allclose(omega_q, omega_qm):
+                    log.error(f"Energy mismatch detected between q = {q} and q' = {qm}")
+                for l in range(self.nmodes):
+                    if not np.allclose(Wq[:,l], np.conj(Wqm[:,l])):
+                        log.error(f"Time-reversal symmetry violation detected between q = {q} and q' = {qm}")
+            for l in range(self.nmodes):
+                Wql = Wq[:,l].reshape(-1,3)
+                Wqml = Wqm[:,l].reshape(-1,3)
+                for k in range(atoms.nat):
+                    self.lph_eq[:,l,i] += -1j*np.cross(Wql[k,:], Wqml[k,:])
+            wq_list.append(omega_q)
+        wq_list = np.array(wq_list)
+        if mpi.rank == mpi.root:
+            plot_lph_struct(wq_list * THz_to_ev * 1.E3, self.lph_eq, n, n_interp=n_interp)
+        mpi.comm.Barrier()
+    def compute_full_ph_angular_momentum_matrix(self, qgr):
+        self.Mph = np.zeros((3,self.nmodes,self.nmodes,qgr.nq), dtype=np.complex128)
+        # compute at each q
+        for i, q in enumerate(qgr.qpts):
+            print(i, q)
+    def read_ph_hamilt(self, qgr):
+        # read ph basis
+        self.read_phonon_basis()
+        # read force matrix from file
+        self.read_force_matrix()
+        # read K points
+        self.compute_energy_dispersion(qgr)
