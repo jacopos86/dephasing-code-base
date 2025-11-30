72429fcb33e7104c7d1a1049da391e4b24444fff
jacopos86 <simonij@tcd.ie>
2025-11-30 11:37:06 -0600

    set q_grid and compute phonons

--- pydephasing/elec_dyn_solvers.py
+++ pydephasing/elec_dyn_solvers.py
@@ -6,6 +6,7 @@ from pydephasing.parallelization.mpi import mpi
 from pydephasing.utilities.log import log
 from pydephasing.wannier_interface.wannier import Wannier
 from pydephasing.phonons_module import JDFTxPhonons
+from pydephasing.q_grid import jdftx_qgridClass
 from pydephasing.set_structs import JDFTxStruct
 from pydephasing.atomic_list_struct import atoms
 
@@ -41,13 +42,17 @@ def solve_elec_dyn_JDFTx_data():
     wan.plot_band_structure()
     if p.dynamical_mode[1] > 0:
         # read phonons data
+        # set q grid
+        qgr = jdftx_qgridClass(p.bnd_kpts_file)
+        qgr.set_qgrid()
         # if we need to compute e-ph interactions
         if mpi.rank == mpi.root:
             log.info("\t " + p.sep)
             log.info("\t COLLECT PHONONS INFORMATION")
             log.info("\t " + p.sep)
             log.info("\n")
-        ph = JDFTxPhonons(p.ph_cellmap_file, p.ph_eigenv_file, p.ph_outfile, p.bnd_kpts_file)
-        ph.read_ph_hamilt()
+        ph = JDFTxPhonons(p.ph_cellmap_file, p.ph_eigenv_file, p.ph_outfile, p.ph_basis_file)
+        ph.read_ph_hamilt(qgr=qgr)
         ph.get_ph_supercell()
-        ph.compute_eq_ph_angular_momentum()
+        ph.compute_eq_ph_angular_momentum_dispersion(qgr)
+        ph.compute_full_ph_angular_momentum_matrix(qgr)
