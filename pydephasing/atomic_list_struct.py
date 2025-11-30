b4b368fbbd0e07d9129aa11e49c5f48bcc86d5a7
jacopos86 <simonij@tcd.ie>
2025-11-30 11:33:16 -0600

    make atoms_jdftx structure

--- pydephasing/atomic_list_struct.py
+++ pydephasing/atomic_list_struct.py
@@ -1,21 +1,19 @@
 import numpy as np
+import os
+import re
 import yaml
-from utilities.log import log
+import mendeleev
+from pydephasing.utilities.input_parser import parser
+from pydephasing.utilities.log import log
 from pydephasing.set_param_object import p
-from parallelization.mpi import mpi
-from common.phys_constants import mp
+from pydephasing.parallelization.mpi import mpi
+from pydephasing.common.phys_constants import mp
+
 # define atomic structure global list
 class AtomicStructureClass:
     # initialization
     def __init__(self):
         self.nat = None
-        # keys
-        self.supercell_key = ''
-        self.unitcell_key = ''
-        self.atoms_pos_key = ''
-        self.lattice_key = ''
-        self.mass_key = ''
-        self.coords_key = ''
         # arrays
         self.atoms_dict = None
         self.index_to_ia_map = None
@@ -25,6 +23,34 @@ class AtomicStructureClass:
         self.supercell_size = None
         self.supercell_grid = None
         self.lattice = None
+    # set index_to_ia_map
+    def compute_index_to_ia_map(self):
+        self.index_to_ia_map = np.zeros(3*self.nat, dtype=int)
+        for jax in range(3*self.nat):
+            self.index_to_ia_map[jax] = int(jax/3)
+    # set index to idx map
+    def compute_index_to_idx_map(self):
+        self.index_to_idx_map = np.zeros(3*self.nat, dtype=int)
+        for jax in range(3*self.nat):
+            self.index_to_idx_map[jax] = jax%3
+    # set atoms mass
+    def set_atoms_mass(self):
+        self.atoms_mass = np.zeros(self.nat)
+        for ia in range(self.nat):
+            m_ia = self.atoms_dict[ia][self.mass_key]
+            self.atoms_mass[ia] = m_ia * mp
+            # eV ps^2 / ang^2
+
+class phonopyAtomicStructureClass(AtomicStructureClass):
+    def __init__(self):
+        super().__init__()
+        # keys
+        self.supercell_key = ''
+        self.unitcell_key = ''
+        self.atoms_pos_key = ''
+        self.lattice_key = ''
+        self.mass_key = ''
+        self.coords_key = ''
     #  get atoms keys
     def get_atoms_keys(self):
         # open file
@@ -67,6 +93,7 @@ class AtomicStructureClass:
                     self.coords_key = k
             if self.coords_key == '':
                 log.error("coordinates key not found")
+    # set number of atoms
     def set_number_of_atoms(self):
         with open(p.yaml_pos_file) as f:
             data = yaml.full_load(f)
@@ -114,22 +141,97 @@ class AtomicStructureClass:
             if mpi.rank == mpi.root:
                 log.info("\t n. supercell grid points: " + str(self.supercell_size))
                 log.info("\t supercell grid: " + str(self.supercell_grid))
-    # set index_to_ia_map
-    def compute_index_to_ia_map(self):
-        self.index_to_ia_map = np.zeros(3*self.nat, dtype=int)
-        for jax in range(3*self.nat):
-            self.index_to_ia_map[jax] = int(jax/3)
-    # set index to idx map
-    def compute_index_to_idx_map(self):
-        self.index_to_idx_map = np.zeros(3*self.nat, dtype=int)
-        for jax in range(3*self.nat):
-            self.index_to_idx_map[jax] = jax%3
-    # set atoms mass
-    def set_atoms_mass(self):
-        self.atoms_mass = np.zeros(self.nat)
-        for ia in range(self.nat):
-            m_ia = self.atoms_dict[ia][self.mass_key]
-            self.atoms_mass[ia] = m_ia * mp
-            # eV ps^2 / ang^2
+
 #
-atoms = AtomicStructureClass()
+#   JDFTx atomic structure
+# 
+
+class JDFTxAtomicStructureClass(AtomicStructureClass):
+    def __init__(self):
+        super().__init__()
+        self.mass_key = "mass"
+        self.pos_pattern = r"ion (\w+) +([0-9\.]+) +([0-9\.]+) +([0-9\.]+)"
+    # set atoms data
+    def set_atoms_data(self):
+        self.set_number_of_atoms()
+        self.compute_index_to_ia_map()
+        self.compute_index_to_idx_map()
+        # extract atoms coordinate
+        self.extract_atoms_coords()
+        # set atoms mass
+        self.set_atoms_mass()
+    # set number of atoms
+    def set_number_of_atoms(self):
+        self.nat = 0
+        with open(p.dft_outfile) as f:
+            data = f.readlines()
+            for line in data:
+                match = re.match(self.pos_pattern, line)
+                if match:
+                    self.nat += 1
+    # set atomic coordinates
+    def extract_atoms_coords(self):
+        keys = ["symbol", "coordinates", "mass"]
+        # read from dft output
+        self.atoms_dict = []
+        with open(p.dft_outfile) as f:
+            data = f.readlines()
+            for line in data:
+                match = re.match(self.pos_pattern, line)
+                if match:
+                    species = match.group(1)
+                    x = float(match.group(2))
+                    y = float(match.group(3))
+                    z = float(match.group(4))
+                    R = np.array([x, y, z])
+                    M = mendeleev.element(species).mass
+                    self.atoms_dict.append(dict(zip(keys, [species, R, M])))
+    def print_atoms_info(self):
+        """
+        Prints the atomic information including species and positions from the extracted atoms_dict.
+        """
+        log.info("\t Atomic Information:")
+        if not self.atoms_dict:
+            log.error("-> No atom information available...")
+        else:
+            for atom in self.atoms_dict:
+                species = atom["symbol"]
+                R = atom["coordinates"]
+                mass = atom["mass"]
+                log.info(f"\t Species: {species}, Position: ({R[0]:.6f}, {R[1]:.6f}, {R[2]:.6f}), Mass: {mass:.6f}")
+            log.info("\t number of atoms: " + str(self.nat))
+
+#  atoms proxy class
+
+class atoms_proxy:
+    def __init__(self):
+        self._real_atoms = None
+    
+    def set_input_arguments(self):
+        is_testing = os.getenv('PYDEPHASING_TESTING') == '1'
+        if is_testing is True:
+            args = parser.parse_args(args=[])
+        else:
+            args = parser.parse_args()
+        return args
+
+    def _init(self):
+        # object initialization
+        args = self.set_input_arguments()
+        co = parser.parse_args().co[0]
+        if co == 'elec-sys':
+            ct2 = args.ct2
+            if ct2 == "jdftx":
+                self._real_atoms = JDFTxAtomicStructureClass()
+            else:
+                # default set phonopy
+                self._real_atoms = phonopyAtomicStructureClass()
+        else:
+            self._real_atoms = phonopyAtomicStructureClass()
+
+    def __getattr__(self, attr):
+        if self._real_atoms is None:
+            self._init()
+        return getattr(self._real_atoms, attr)
+
+atoms = atoms_proxy()
