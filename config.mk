826d9df24f0c08c686733a6ac07b930dcfd4b156
jacopos86 <simonij@tcd.ie>
2025-12-24 23:11:57 -0600

    include other env variables

--- config.mk
+++ config.mk
@@ -25,3 +25,29 @@ INSTALL_PYCUDA ?= 0
 
 PETSC_DIR ?=
 PETSC_ARCH ?=
+
+# ===================
+#  TESTS dir.
+# ===================
+
+TESTS_DIR := $(ROOT)/TESTS
+
+# ===================
+#   tar.gz files url
+# ===================
+
+EXAMPLES_URL := "https://drive.google.com/file/d/1ueLGCuRSZO-c1hwrCvhO913TyBTjkuP9/view?usp=sharing&confirm=t"
+TESTS_3_URL := "https://drive.google.com/file/d/1Vv_xmpivm8p0vjsTG0MIlB2Th7GykTQk/view?usp=drive_link"
+
+# ===================
+# Skipping downloads
+# ===================
+
+DOWNLOAD_EXAMPLES ?= 1
+DOWNLOAD_TESTS3 ?= 1
+
+# ===================
+#  Log level
+# ===================
+
+LOG_LEVEL := INFO
