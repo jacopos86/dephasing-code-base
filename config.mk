770a6474487e1ae730f5c1cda35d234e9987c2b2
jacopos86 <simonij@tcd.ie>
2025-12-27 21:15:16 -0600

    file with environment variables

--- config.mk
+++ config.mk
@@ -18,6 +18,9 @@ BUILD_MODE ?= local
 # ===================
 
 INSTALL_PYCUDA ?= 0
+GPU_ACTIVE ?= 0
+GRID_SIZE ?=
+BLOCK_SIZE ?=
 
 # ===================
 #  PETSc variables
@@ -27,17 +30,29 @@ PETSC_DIR ?=
 PETSC_ARCH ?=
 
 # ===================
+#  unit tests
+# ===================
+
+NP_MAX ?= 2
+UNIT_TEST_DIR := $(ROOT)/pydephasing/unit_tests
+
+# ===================
 #  TESTS dir.
 # ===================
 
+BUILD_TESTS ?= "1 2"
 TESTS_DIR := $(ROOT)/TESTS
+TESTS_12_TAR_FILE := $(ROOT)/EXAMPLES.tar.gz
+TESTS_3_TAR_FILE := $(ROOT)/TESTS_3.tar.gz
 
 # ===================
 #   tar.gz files url
 # ===================
 
 EXAMPLES_URL := "https://drive.google.com/file/d/1ueLGCuRSZO-c1hwrCvhO913TyBTjkuP9/view?usp=sharing&confirm=t"
+EXAMPLES_FILE_ID := 1ueLGCuRSZO-c1hwrCvhO913TyBTjkuP9
 TESTS_3_URL := "https://drive.google.com/file/d/1Vv_xmpivm8p0vjsTG0MIlB2Th7GykTQk/view?usp=drive_link"
+TESTS_3_FILE_ID := 1Vv_xmpivm8p0vjsTG0MIlB2Th7GykTQk
 
 # ===================
 # Skipping downloads
@@ -50,4 +65,6 @@ DOWNLOAD_TESTS3 ?= 1
 #  Log level
 # ===================
 
-LOG_LEVEL := INFO
+LOG_LEVEL ?= INFO
+COLOR_LOG ?= 1
+LOG_FILE ?= out.log
