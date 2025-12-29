3af4dd42ea3c6ccb21a2d72462b0af3e264e0f69
jacopos86 <simonij@tcd.ie>
2025-12-28 22:44:20 -0600

    build GS elec struct

--- pydephasing/build_unpert_struct.py
+++ pydephasing/build_unpert_struct.py
@@ -1,4 +1,5 @@
 from pydephasing.set_structs import UnpertStruct
+from pydephasing.set_structs import JDFTxStruct
 from pydephasing.set_param_object import p
 
 #
@@ -23,9 +24,16 @@ def build_gs_spin_struct(gs_data_dir, HFI_CALC):
         struct0.set_hfi_Dbasis(p.fc_core)
     return struct0
 
-def build_gs_elec_struct(gs_data_dir):
+def build_vasp_gs_elec_struct(gs_data_dir):
     struct0 = build_gs_struct_base(gs_data_dir)
     # extract energy eigenvalues
     struct0.extract_energy_eigv()
     struct0.extract_orbital_character()
+    struct0.read_chem_pot_from_data()
+    return struct0
+
+def build_jdftx_gs_elec_struct(gs_data_dir):
+    struct0 = JDFTxStruct(gs_data_dir)
+    # set up electronic parameters
+    struct0.set_elec_parameters()
     return struct0
