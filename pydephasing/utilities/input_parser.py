475c2f4a64c8b4a661fa9ee6af74f7bee4ae3204
jacopos86 <simonij@tcd.ie>
2026-01-18 09:30:02 -0600

    input_parser / reading_files moved to input section

--- pydephasing/utilities/input_parser.py
+++
@@ -1,9 +0,0 @@
-import argparse
-# set up parser
-# make parser global to read parameters
-parser = argparse.ArgumentParser()
-parser.add_argument('-ct1', nargs=1, default=['init'])
-parser.add_argument('-co', nargs=1, default=None)
-parser.add_argument('-o', nargs='?', default='1')
-parser.add_argument('-ct2', nargs='?', default=None)
-parser.add_argument('-yml_inp', nargs=1, default=None)
