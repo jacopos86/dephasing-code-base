3825aa8589bcda467106c1afc62a9aa546c9e999
jacopos86 <simonij@tcd.ie>
2026-01-30 10:08:29 -0600

    initialize Ehrenfest field

--- pydephasing/Ehrenfest_field.py
+++ pydephasing/Ehrenfest_field.py
@@ -3,3 +3,12 @@
 #   define Ehrenfest field class
 #
 
+class ehr_field(object):
+    # initialization
+    def __init__(self):
+        self.nqpt = None
+        self.nmd = None
+    def initialize_Bfield(self, nqpt, nmd):
+        self.nqpt = nqpt
+        self.nmd = nmd
+        # define displ. field
