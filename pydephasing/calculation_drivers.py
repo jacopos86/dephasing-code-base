9b53580c0c63af781df2f8dce05909d50092cfeb
jacopos86 <simonij@tcd.ie>
2026-02-13 13:33:11 -0600

    remove QUANTUM

--- pydephasing/calculation_drivers.py
+++ pydephasing/calculation_drivers.py
@@ -5,7 +5,6 @@ from pydephasing.set_param_object import p
 from pydephasing.compute_LR_spin_decoher import compute_spin_dephas
 from pydephasing.compute_hfi_dephas_stat import compute_hfi_stat_dephas
 from pydephasing.real_time_spin_dephas_solver import compute_RT_spin_dephas
-from pydephasing.quantum.compute_QA_spin_decoher import compute_dephas_QA
 from pydephasing.elec_dyn_solvers import solve_elec_dyn_VASP_data, solve_elec_dyn_JDFTx_data, solve_elec_model_dyn
 from pydephasing.compute_LR_elec_decoher import compute_VASP_elec_dephas, compute_JDFTx_elec_dephas
 
@@ -154,7 +153,7 @@ def spin_qubit_driver(yml_file):
                 log.info("\n")
                 log.info("\t " + p.sep)
                 log.warning("\t CODE USAGE: \n")
-                log.warning("\t -> python pydephasing -ct1 [LR, RT, QUANTUM, init, postproc] -co [spin-qubit, energy-lw] -ct2 [inhomo,stat,statdd,homo,full] - yml_inp [input]")
+                log.warning("\t -> python pydephasing -ct1 [LR, RT, init, postproc] -co [spin-qubit, energy-lw] -ct2 [inhomo,stat,statdd,homo,full] - yml_inp [input]")
                 log.info("\t " + p.sep)
             log.error("\t calc_type2 wrong: " + calc_type2)
         #
@@ -235,43 +234,6 @@ def spin_qubit_driver(yml_file):
         #  START NON MARKOVIAN CALCULATION
         #
         T2_calc_handler = compute_RT_spin_dephas(ZFS_CALC, HFI_CALC)
-    elif calc_type1 == "QUANTUM":
-        if mpi.rank == mpi.root:
-            log.info("\n")
-            log.info("\t " + p.sep)
-            log.info("\n")
-            log.info("\t " + " PERFORM QUANTUM CALCULATION")
-            log.info("\n")
-            log.info("\t " + p.sep)
-            log.info("\n")
-        # read input file
-        p.read_yml_data(yml_file)
-        # compute auto correl. function first
-        if calc_type2 == "homo":
-            ZFS_CALC = True
-            HFI_CALC = False
-        elif calc_type2 == "inhomo":
-            ZFS_CALC = False
-            HFI_CALC = True
-        elif calc_type2 == "full":
-            ZFS_CALC = True
-            HFI_CALC = True
-        else:
-            if mpi.rank == mpi.root:
-                log.info("\n")
-                log.info("\t " + p.sep)
-                log.warning("\t CODE USAGE: \n")
-                log.warning("\t -> python pydephasing -ct1 [LR, RT, QUANTUM, init, postproc] -co [spin-qubit, energy-lw] -ct2 [inhomo,stat,statdd,homo,full] - yml_inp [input]")
-                log.info("\t " + p.sep)
-            log.error("\t calc_type2 wrong: " + calc_type2)
-        if mpi.rank == mpi.root:
-            log.info("\n")
-            log.info("\t T2 CALCULATION -> STARTING")
-            log.info("\t ZFS_CALC: " + str(ZFS_CALC))
-            log.info("\t HFI_CALC: " + str(HFI_CALC))
-            log.info("\n")
-            log.info("\t " + p.sep)
-        T2_calc_handler = compute_dephas_QA(ZFS_CALC, HFI_CALC)
     else:
         if mpi.rank == mpi.root:
             log.info("\n")
